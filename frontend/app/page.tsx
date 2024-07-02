1111ec909a68e642f295b5bb4d19527beb43715b
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-07-02 17:01:35 -0300

    feat: add components to home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,18 @@
 import Hero from "@/components/Hero";
+import NewCollections from "@/components/NewCollections";
+import NewsLetter from "@/components/NewsLetter";
+import Offer from "@/components/Offer";
+import Popular from "@/components/Popular";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Popular />
+      <Offer />
+      <NewCollections />
+      <NewsLetter />
     </main>
   );
 }
